df63e88852a97c69e9e5097fa3edc691a8947afa
Necromancer <saizen777999@gmail.com>
2025-05-30 22:41:39 +0530

    Js_002

--- Log_folder/ip_auth_mdw&zod.js
+++ Log_folder/ip_auth_mdw&zod.js
@@ -1,10 +1,18 @@
-// Cool way of doing input validation using mdw and zod.
+// Js_002: Cool way of doing input validation using mdw and zod.
+
+/* express.json() → its the middleware to parse the request body
+zod → Validation tool to check the request body structure and types
+They work together:
+1. Middleware makes the data available
+2. Zod ensures the data is valid, Zod itself is not a middleware. 
+It is a schema declaration and validation library that is used to define the expected structure of data and thus validate if the incoming data matches that structure or not.*/
+
 const zod = require("zod"); 
 const express = require("express");
 const app = express();
 
-const schema = zod.array(zod.number()); // zod schema is any kind of data type 
-//app.use(express.json())  // this is a middleware that parses the body of the request and converts it into a json object.
+const schema = zod.array(zod.number());     // zod schema is any kind of data type 
+//app.use(express.json())         // this is a middleware that parses the body of the request, converts it into a json object and attaches it to req.body.
 //app.post("/hlth_chkup", (req, res)=>{
     // input kidneys is expected as an array.
     //const kidneys = req.body.kidneys;  // this is the body of the request which will be parsed by middleware.
@@ -44,6 +52,5 @@ app.post("/login",(req, res) => {
 // app.use((err, req, res, next)=>{
 //     res.send("An error has occured")
 // })
-
 
 app.listen(3000);
