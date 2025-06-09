fc3d7ca856910c45fc692da423136142314dfa69
Necromancer <saizen777999@gmail.com>
2025-06-09 23:58:44 +0530

    Create pagination.js

---
+++ Log_folder/pagination.js
@@ -0,0 +1,54 @@
+/* Js_011: Lets implement pagination and filtering task.
+
+Pagination is the process of dividing content or data into discrete pages. It's a common technique used in web and app interfaces when displaying a large dataset like search results, product listings, or user comments to improve performance and user experience.
+
+Here, user data is split into manageable pages using query parameters like page and limit, enabling efficient retrieval of results. Filtering is supported via a search parameter, which performs a case-insensitive match on first name, last name, or email fields.*/
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const User = require("./models/user");
+
+const app = express();
+const userRoutes = require("./routes/user");
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.post("/api/users", async (req, res) => {
+  try {
+    const { first_name, last_name, email, gender, job_title, status } =
+      req.body;
+
+    const newUser = new User({
+      first_name,
+      last_name,
+      email,
+      gender,
+      job_title,
+      status,
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "User creation failed", details: err.message });
+  }
+}); // Postman can be used to send a post req and create a user
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on Port: ${PORT}`);
+});
